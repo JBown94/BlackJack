e3a95c27d216d7a48b7ac136249d414a96c07f89
Josh Bown <joshbown.94@outlook.com>
2020-07-28 13:55:54 +0100

    Added the foundation for the drag & drop UI events

--- src/Card.js
+++ src/Card.js
@@ -29,13 +29,34 @@ class Card extends React.Component {
         }
     }
 
+    dragStart(e) {
+        console.log("Drag Start");
+        console.log(this);
+    }
+    dragEnd(e) {
+        console.log("Drag End");
+        console.log(this);
+
+        //TODO: Check if the drop target is the playing area (or it's container), then
+        //      Remove it from the player area & add it to the start of the cardsInPlay
+
+        this.setState({
+            hidden: true,
+            selected: false,
+            draggable: false
+        });
+    }
+
     render() {
       return (
         <div className="card" data-value={this.state.value} data-suit={this.state.suit}
-            data-selected={this.state.selected}
-            data-hidden={this.state.hidden}
+            data-selected={this.state.selected} data-hidden={this.state.hidden}
             draggable={this.state.draggable}
+            
+            onDragStart={e => this.dragStart(e)}
+            onDragEnd={e => this.dragEnd(e)}
             onClick={() => this.toggleSelection()}>
+            
             <div className="card-val">{this.state.value}</div>
             <div className="suit-img"></div>
             <div className="card-val-reverse">{this.state.value}</div>
